32b9c1f542f5cabf295bd76af40bec7a719171ec
Monosriz Dutta <monosrizms@gmail.com>
2021-03-22 08:20:24 +0100

    Prepartion of project skeleton.

---
+++ WearableDevice/WearableDevice.AppServices/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WearableDevice.AppServices
+{
+    public class Class1
+    {
+    }
+}
---
+++ WearableDevice/WearableDevice.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WearableDevice.Model
+{
+    public class Class1
+    {
+    }
+}
