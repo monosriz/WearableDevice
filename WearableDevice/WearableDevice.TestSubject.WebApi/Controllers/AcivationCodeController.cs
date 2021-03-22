8c82a7460d66b46019b50bfca66de0e81071e860
Monosriz Dutta <monosrizms@gmail.com>
2021-03-22 10:04:44 +0100

    Commit API module activation code added generate activation code method.

---
+++ WearableDevice/WearableDevice.TestSubject.WebApi/Controllers/AcivationCodeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WearableDevice.AppServices;
+
+namespace WearableDevice.TestSubject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AcivationCodeController : ControllerBase
+    {
+        ApplicationWearableDeviceService _applicationWearableDeviceService;
+
+        public AcivationCodeController(ApplicationWearableDeviceService applicationWearableDeviceService)
+        {
+            _applicationWearableDeviceService = applicationWearableDeviceService;
+        }
+
+       /// <summary>
+       /// This method used to genearate activation code 
+       /// </summary>
+       /// <returns></returns>
+        [HttpGet()]
+        [Route("GenerateActivationCode")]
+        public IActionResult GenerateActivationCode()
+
+        {
+
+           
+
+                var _reponse = _applicationWearableDeviceService.GenerateActivationCode();
+                if (_reponse.Success == true)
+                    return Ok(_reponse);
+                else
+                    return BadRequest(Response);
+           
+
+        }
+    }
+}
